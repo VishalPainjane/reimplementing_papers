b36a33266480a302129c8ef160ff7094c820ade0
VishalPainjane <painjanevishal2204@gmail.com>
2025-09-13 02:00:09 +0530

    data capture

--- Diffution_model/init.py
+++ Diffution_model/init.py
@@ -10,3 +10,17 @@ import math
 DEVICE = "cude" if torch.cuda.is_available() else "cpu"
 
 print(f'Using device {DEVICE}')
+
+img_size = 32
+batch_size = 128
+
+transforms = v2.compose([
+    v2.Resize((img_size, img_size)), # resize to 32*32
+    v2.ToTensor(),
+    v2.Lambda(lambda t:(t*2)-1) # change all the values into the range [-1.0, 1.0]
+    ])
+
+dataset = torchvision.datasets.CIFAR10(root='./data', download=True, transform=transforms)
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+
+
